34fff575b3a7aad8136de82269c1145dc2072ba2
Danilo S. Carvalho <danilo.carvalho@manchester.ac.uk>
2024-05-16 17:26:52 +0100

    - SpanishWordNetFilteredDataSet: Changed treatment of empty entries. - SpanishWordNetFilteredDataSet: Added documentation. - Reorganized imports.

--- saf_datasets/data_access/wordnet_spanish.py
+++ saf_datasets/data_access/wordnet_spanish.py
@@ -11,12 +11,25 @@ URL = BASE_URL + "Spanish_WordNet_filtered_data.jsonl.gz"
 
 
 class SpanishWordNetFilteredDataSet(SentenceDataSet):
+    """
+        Spanish WordNet filtered data set: A collection of filtered definition sentences from the Spanish WordNet
+
+        Each element of this dataset is a definition sentence (gloss) of a single synset, as it appears in WordNet.
+        Definitions were filtered to only include those which definiendum (the term being defined) is representable by
+        a single token from the LLaMa tokenizer.
+        Each sentence is annotated with a "definiendum" and corresponding metadata from WordNet (WordNet id,
+        cess_esp_frequencies, wordnet_frequency, category, abstraction_level, generalization_level).
+
+    """
     def __init__(self, path: str = PATH, url: str = URL):
         super(SpanishWordNetFilteredDataSet, self).__init__(path, url)
         with gzip.open(self.data_path, 'rt') as dataset_file:
             with jsonlines.Reader(dataset_file) as reader:
                 self.data = list()
                 for line in tqdm(reader, desc="Loading WordNet filtered data"):
+                    if (not line["definition"]):
+                        continue
+
                     sentence = Sentence()
                     sentence.surface = line["definition"]
                     sentence.annotations["definiendum"] = line["name"]
@@ -27,15 +40,13 @@ class SpanishWordNetFilteredDataSet(SentenceDataSet):
                     sentence.annotations["generalization_level"] = line["generalization_level"]
                     sentence.annotations['id'] = line["id"]
                     sentence.annotations['definition_src'] = line['definition_src']
-                    if line['definition'] is not None:
-                        for tok in line["definition_attributes"]:
-                            token = Token()
-                            token.surface = tok['name']
-                            token.annotations = tok #add the ID to the token annotations
-                            sentence.tokens.append(token)
-                    else:
-                        sentence.surface = 'None'
-                        sentence.tokens = []
+
+                    for tok in line["definition_attributes"]:
+                        token = Token()
+                        token.surface = tok['name']
+                        token.annotations = tok #add the ID to the token annotations
+                        sentence.tokens.append(token)
+
                     self.data.append(sentence)
 
     def __iter__(self):
