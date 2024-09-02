be96b4eee7481d71f0d72a6c0d97f96720eb8e61
Nura <nura.aljaafari@postgrad.manchester.ac.uk>
2024-09-02 16:29:24 +0100

    Adding the Formalised WordNet Class

---
+++ saf_datasets/data_access/wordnet_formalised.py
@@ -0,0 +1,62 @@
+import gzip
+import jsonlines
+from tqdm import tqdm
+from saf import Sentence, Vocabulary
+from .dataset import SentenceDataSet, BASE_URL
+
+PATH = "/Users/user/Desktop/Code/Definitions and concepts/concepts_formalisation/data/sample_concept/48/improved_definitions_combined_data_15_39_48_49_57_.jsonl.gz"
+URL = "/Users/user/Desktop/Code/Definitions and concepts/concepts_formalisation/data/sample_concept/48/improved_definitions_combined_data_15_39_48_49_57_.jsonl.gz"
+
+
+ANNOT_RESOURCES = {
+    "pos+lemma+ctag+dep+dsr+srl": {
+        "path": "WordNet/wordnet_filtered_spacy_dsr_srl.pickle.gz",
+        "url": BASE_URL + "wordnet_filtered_spacy_dsr_srl.pickle.gz"
+    }
+}
+
+
+class WordNetFormalisedDataSet(SentenceDataSet):
+    def __init__(self, path: str = PATH, url: str = URL):
+        super(WordNetFormalisedDataSet, self).__init__(path, url)
+        with gzip.open(self.data_path, 'rt') as dataset_file:
+            print('path', self.data_path)
+            with jsonlines.Reader(dataset_file) as reader:
+                self.data = list()
+                for line in tqdm(reader, desc="Loading WordNet formalised data"):
+                    sentence = Sentence()
+                    sentence.annotations['id'] = line['id']
+                    sentence.annotations['definiendum'] = line['definiendum']
+                    sentence.annotations['features'] = line['definition']
+                    sentence.surface = self.format_definition(line['features'])
+                    self.data.append(sentence)
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx: int) -> Sentence:
+        return self.data[idx]
+
+    def format_definition(self, characteristics, conjunction='AND', prompt='A term that {char_string} is called \"'):
+        formatted_chars = [f"{char['predicate']} {char['object']}" for char in characteristics]
+        if conjunction == 'AND':
+            char_string = " AND ".join(formatted_chars[:-1])
+        else:
+            char_string = ", ".join(formatted_chars[:-1])
+
+        # Add the last characteristic with "and"
+        if len(formatted_chars) > 1:
+            char_string += f" AND {formatted_chars[-1]}"
+        else:
+            char_string = formatted_chars[0]
+
+        # Create the full prompt
+        prompt = f"{prompt}{char_string}\""
+
+        return prompt
+
+
+w = WordNetFormalisedDataSet()
