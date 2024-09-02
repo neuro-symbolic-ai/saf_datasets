864519a6a97b5e47480ead9747a630f3ba2ff968
Nura <nura.aljaafari@postgrad.manchester.ac.uk>
2024-09-02 17:13:44 +0100

    Added the hypernym to annotations

--- saf_datasets/data_access/wordnet_formalised.py
+++ saf_datasets/data_access/wordnet_formalised.py
@@ -17,6 +17,10 @@ ANNOT_RESOURCES = {
 
 
 class WordNetFormalisedDataSet(SentenceDataSet):
+    '''
+    Wordnet filtered data set: A collection of formalised definition sentences from WordNet
+    Each element of this dataset is a definition sentence (gloss) of a single synset, as it appears in WordNet.
+    '''
     def __init__(self, path: str = PATH, url: str = URL):
         super(WordNetFormalisedDataSet, self).__init__(path, url)
         with gzip.open(self.data_path, 'rt') as dataset_file:
@@ -28,6 +32,7 @@ class WordNetFormalisedDataSet(SentenceDataSet):
                     sentence.annotations['id'] = line['id']
                     sentence.annotations['definiendum'] = line['definiendum']
                     sentence.annotations['features'] = line['definition']
+                    sentence.annotations['hypernym'] = line['hypernym']
                     sentence.surface = self.format_definition(line['features'])
                     self.data.append(sentence)
 
@@ -40,7 +45,7 @@ class WordNetFormalisedDataSet(SentenceDataSet):
     def __getitem__(self, idx: int) -> Sentence:
         return self.data[idx]
 
-    def format_definition(self, characteristics, conjunction='AND', prompt='A term that {char_string} is called \"'):
+    def format_definition(self, characteristics, conjunction='AND', prompt='A term that {char_string} is called \"') -> str:
         formatted_chars = [f"{char['predicate']} {char['object']}" for char in characteristics]
         if conjunction == 'AND':
             char_string = " AND ".join(formatted_chars[:-1])
@@ -59,4 +64,4 @@ class WordNetFormalisedDataSet(SentenceDataSet):
         return prompt
 
 
-w = WordNetFormalisedDataSet()
+# w = WordNetFormalisedDataSet()
